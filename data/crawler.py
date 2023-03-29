094935be7f24bc7f24799c86afc30d52f3b16443
yucheng <yucheng.li@surrey.ac.uk>
2023-03-29 22:39:14 +0100

    server update

--- data/crawler.py
+++ data/crawler.py
@@ -3,6 +3,10 @@ import sys
 from glob import glob
 import tarfile
 from parse_tex import pruned_latex_to_text
+from tqdm import tqdm
+import os
+import traceback
+import shutil
 
 if __name__ == '__main__':
     output_dir = sys.argv[1]
@@ -11,18 +15,21 @@ if __name__ == '__main__':
         query='submittedDate:[20230301181133 TO 20230329181133]',
         max_results = 1000,
         sort_by = arxiv.SortCriterion.SubmittedDate,
-        sort_order=SortOrder.Descending
+        sort_order=arxiv.SortOrder.Descending
     )
 
-    for result in search.results():
-        result.download_source(output_dir)
+    # print('num of search results', len(search.results()))
+    text_save_dir = os.path.join(output_dir, 'text')
+    if not os.path.exists(text_save_dir):
+        os.makedirs(text_save_dir)
+
+    count = 1
+    for result in tqdm(search.results()):
+        result.download_source(output_dir, filename = f'{count}.arxiv_source')
     
-    os.mkdir(os.path.join(output_dir, 'text'))
-    
-    for source_file in os.listdir(output_dir):
-        count = 1
         try:
-            path = os.path.join(output_dir, count)
+            path = os.path.join(output_dir, str(count))
+            source_file = os.path.join(output_dir, f'{count}.arxiv_source')
             with tarfile.open(source_file) as tar:
                 tar.extractall(path)
 
@@ -30,16 +37,20 @@ if __name__ == '__main__':
             tex_files = [file for file in extracted_files if file.endswith('.tex')]
             if len(tex_files)>1:
                 print('Multiple tex files for', source_file)
-                os.rmdir(path)
+                shutil.rmtree(path)
+                continue
             tex_file = tex_files[0]
-            with open(tex_file) as f:
+            with open(os.path.join(path, tex_file)) as f:
                 text = pruned_latex_to_text(f.read())
             
-            text_save_path = os.path.join(output_dir, 'text', count)
+            text_save_path = os.path.join(output_dir, 'text', str(count))
             with open(text_save_path, 'w') as f:
                 f.write(text)
 
+            shutil.rmtree(path)
+            os.remove(source_file)
             count += 1
         except:
+            traceback.print_exc()
             continue
     
